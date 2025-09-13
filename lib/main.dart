1f22770b77ae6b58718c0fee9c4db824f721c550
Dineth Siriwardhana <dinethsiriwardana28@gmail.com>
2025-09-13 13:05:14 +0530

    Add Todo model and service classes for data management

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:todo_demo/view/home.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,74 +14,6 @@ class MyApp extends StatelessWidget {
       title: "Flutter demo",
       theme: ThemeData(primarySwatch: Colors.blue),
       home: HomePage(),
-    );
-  }
-}
-
-class HomePage extends StatefulWidget {
-  const HomePage({super.key});
-
-  @override
-  State<HomePage> createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  int count = 0;
-
-  void increment() {
-    setState(() {
-      count++;
-      // Update the UI
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          "My App",
-          style: TextStyle(
-            color: Colors.white,
-            fontSize: 20,
-            fontWeight: FontWeight.bold,
-          ),
-        ),
-        backgroundColor: Colors.blue,
-        leading: Icon(Icons.menu),
-        actions: [
-          IconButton(
-            icon: Icon(Icons.search),
-            onPressed: () {
-              // Action for search button
-            },
-          ),
-          IconButton(
-            icon: Icon(Icons.more_vert),
-            onPressed: () {
-              // Action for more options button
-            },
-          ),
-        ],
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              "Count is $count",
-              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                increment();
-                print(count);
-              },
-              child: Text("Increment", style: TextStyle(fontSize: 20)),
-            ),
-          ],
-        ),
-      ),
     );
   }
 }
---
+++ lib/services/todo_service.dart
@@ -0,0 +1,12 @@
+class TodoService {
+  final String baseUrl =
+      'https://todo-test-app-bvamb3h2hrgsarer.eastus2-01.azurewebsites.net/api';
+
+  // Add methods for CRUD operations here
+
+  final header = {
+    'Content-Type': 'application/json',
+    'Accept': 'application/json',
+    'user-id': "user1",
+  };
+}
